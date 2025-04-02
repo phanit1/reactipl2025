b4f842bf26b2db1c345e732954995c23b1d2a701
Thota Phaneendhar <babithota5@gmail.com>
2025-04-02 21:33:52 +0530

    changes done

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
 const cors = require('cors');
+const puppeteer = require('puppeteer-core');
+const chrome = require('@sparticuz/chrome-aws-lambda');
+
 
 const app = express();
 app.use(cors());
@@ -10,7 +13,29 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/iplmatches', async (req, res) => {
-    const browser = await puppeteer.launch();
+
+        // const browser = await puppeteer.launch({
+        //     args: chromium.args,
+        //     executablePath: await chromium.executablePath,
+        //     headless: chromium.headless,
+        // });
+
+        // Your Puppeteer code here...
+
+        // await browser.close();
+        // res.status(200).send('Puppeteer execution successful.');
+
+
+
+    // const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
+        defaultViewport: chrome.defaultViewport,
+        executablePath: await chrome.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+      });
+
     const page = await browser.newPage();
     await page.goto('https://www.cricbuzz.com/cricket-series/9237/indian-premier-league-2025/matches');
     
@@ -21,7 +46,20 @@ app.get('/api/iplmatches', async (req, res) => {
 });
 
 app.get('/api/iplpoints', async (req, res) => {
-    const browser = await puppeteer.launch();
+    // const browser = await puppeteer.launch();
+    console.log(chromium.executablePath,"exe")
+    // const browser = await puppeteer.launch({
+    //     args: chromium.args,
+    //     executablePath: await chromium.executablePath,
+    //     headless: chromium.headless,
+    // });
+    const browser = await puppeteer.launch({
+        args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
+        defaultViewport: chrome.defaultViewport,
+        executablePath: await chrome.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+      });
     const page = await browser.newPage();
     await page.goto('https://www.cricbuzz.com/cricket-series/9237/indian-premier-league-2025/points-table');
     
