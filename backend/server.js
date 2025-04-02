802049f215fe11f47997341cab55213f53ae4f36
Thota Phaneendhar <babithota5@gmail.com>
2025-04-02 19:59:56 +0530

    added get sample route

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,16 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send('Hello from the server! Here you can find IPL 2025 data.');
+});
+
 app.get('/api/iplmatches', async (req, res) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://www.cricbuzz.com/cricket-series/9237/indian-premier-league-2025/matches');
     
     const content = await page.content();
-    console.log(content, "Content");
     await browser.close();
     
     res.send(content);
@@ -23,7 +26,6 @@ app.get('/api/iplpoints', async (req, res) => {
     await page.goto('https://www.cricbuzz.com/cricket-series/9237/indian-premier-league-2025/points-table');
     
     const content = await page.content();
-    console.log(content, "Content");
     await browser.close();
     
     res.send(content);
