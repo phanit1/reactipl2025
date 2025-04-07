f31f388a302631c46c5f280769b646f91a332706
Thota Phaneendhar <babithota5@gmail.com>
2025-04-07 20:06:45 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -191,11 +191,10 @@ app.post("/api/login", async (req, res) => {
 
     try {
         const user = await User.findOne({ email });
+        const isMatch = password === user.password;
         if (!user) {
             return res.status(400).json({ success: false, message: "Invalid email or password" });
         }
-
-        const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
             return res.status(400).json({ success: false, message: "Invalid email or password" });
         }
