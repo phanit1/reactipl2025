2dcb1cc1e5b1a893b14518b344ec6e86d66e8195
Thota Phaneendhar <babithota5@gmail.com>
2025-04-07 12:54:45 +0530

    points table responsiveness done

--- src/IPLPointsTable.js
+++ src/IPLPointsTable.js
@@ -1,41 +1,40 @@
 import React from "react";
 import Table from "react-bootstrap/Table";
-// import "./IplPointsTable.css";
+import "./IplPointsTable.css"; // Add custom styles here
 
 const IplPointsTable = ({ points }) => {
   return (
-    <div className="container">
-      <Table responsive bordered hover striped className="points-table">
-        <thead>
-          <tr>
-            <th>POS</th>
-            <th>TEAM</th>
-            <th>P</th>
-            <th>W</th>
-            <th>L</th>
-            <th>T</th>
-            <th>NRR</th>
-            <th>PTS</th>
-          </tr>
-        </thead>
-        <tbody>
-          {points.map((team, index) => (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              <td className="team-cell">
-                {/* <img src={team.teamLogo} alt={team.team} className="team-logo" /> */}
-                <span>{team.team}</span>
-              </td>
-              <td>{team.matchesPlayed}</td>
-              <td>{team.matchesWon}</td>
-              <td>{team.matchesLost}</td>
-              <td>{team.matchesTied}</td>
-              <td>{team.netRunRate}</td>
-              <td className="points">{team.points}</td>
+    <div className="container-fluid">
+        <Table bordered hover striped className="points-table">
+          <thead>
+            <tr>
+              <th>POS</th>
+              <th>TEAM</th>
+              <th>P</th>
+              <th>W</th>
+              <th>L</th>
+              <th>T</th>
+              <th>NRR</th>
+              <th>PTS</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {points.map((team, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td className="team-cell">
+                  <span>{team.team}</span>
+                </td>
+                <td>{team.matchesPlayed}</td>
+                <td>{team.matchesWon}</td>
+                <td>{team.matchesLost}</td>
+                <td>{team.matchesTied}</td>
+                <td>{team.netRunRate}</td>
+                <td className="points">{team.points}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
     </div>
   );
 };
